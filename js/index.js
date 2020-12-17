8fe47b45500491aaf754defff91116991534cf8b
Coen Mathijssen <mathijssen.coen@gmail.com>
2020-12-17 14:11:45 +0100

    playground for mapbox

---
+++ js/index.js
@@ -0,0 +1,11 @@
+// const mapboxgl = require('mapbox-gl')
+// console.log(mapboxgl)
+
+// mapboxgl.accessToken = 'pk.eyJ1IjoiY29lbm1hdCIsImEiOiJja2lzb2djNWYwdDhqMnRvYmNwcjZuZ2dhIn0.RVhGR_MZC56vLvzXvvDENw';
+// const map = new mapboxgl.Map({
+// container: 'map',
+// style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
+// center: [-74.5, 40], // starting position [lng, lat]
+// zoom: 9 // starting zoom
+// });
+
